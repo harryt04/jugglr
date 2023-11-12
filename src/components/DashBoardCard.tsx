a0eea6a33b94fc959c903db5bedb7055ace41167
Harry Thomas <hsthomas@verisk.com>
2023-11-12 10:40:50 -0700

    time picker mockup init

--- src/components/DashBoardCard.tsx
+++ src/components/DashBoardCard.tsx
@@ -17,12 +17,12 @@ export const DashBoardCard = () => {
         <Typography sx={{ mb: 1.5 }} color="text.secondary">
           3 active orders
         </Typography>
-        <ItemProgress value={55} label={"Steak 1"} remainingSeconds={345} />
-        <ItemProgress value={20} label={"Steak 2"} remainingSeconds={234} />
+        <ItemProgress value={55} label={"Steak 1"} remainingseconds={345} />
+        <ItemProgress value={20} label={"Steak 2"} remainingseconds={234} />
         <ItemProgress
           value={70}
           label={"Ceasar Salad 1"}
-          remainingSeconds={123}
+          remainingseconds={123}
         />
       </CardContent>
     </Card>
--- src/components/ItemProgress.tsx
+++ src/components/ItemProgress.tsx
@@ -3,19 +3,24 @@ import LinearProgress, {
   LinearProgressProps,
 } from "@mui/material/LinearProgress";
 import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
 import { formatTime } from "../utils/unitConversion";
 import { IconButton } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
+import CloseIcon from "@mui/icons-material/Close";
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
+import { Moment } from "moment";
 
 export const ItemProgress = (
   props: LinearProgressProps & {
     value: number;
     label: string;
-    remainingSeconds: number;
+    remainingseconds: number;
   },
 ) => {
+  const [isOpen, setIsOpen] = React.useState(false);
   const styles = {
     container: {
       width: "100%",
@@ -29,7 +34,7 @@ export const ItemProgress = (
       display: "grid",
       justifyContent: "center",
       alignItems: "center",
-      gridTemplateColumns: "1fr auto auto",
+      gridTemplateColumns: "1fr auto auto auto auto",
     },
   };
 
@@ -42,12 +47,30 @@ export const ItemProgress = (
             props.value,
           )}% ${props.label}`}</Typography>
           <Typography variant="body2" color="text.disabled">{`${formatTime(
-            props.remainingSeconds,
+            props.remainingseconds,
           )} remaining`}</Typography>
         </div>
-        <IconButton aria-label="edit" color="primary">
-          <EditIcon />
+        <IconButton
+          aria-label="edit"
+          color="primary"
+          onClick={() => {
+            setIsOpen(!isOpen);
+          }}>
+          {isOpen && <CloseIcon />}
+          {!isOpen && <EditIcon />}
         </IconButton>
+        {isOpen && (
+          <LocalizationProvider dateAdapter={AdapterMoment}>
+            <TimePicker
+              views={["minutes", "seconds"]}
+              format="mm:ss"
+              onChange={(newValue: Moment | null) => {
+                if (!newValue) return;
+                console.log("newValue: ", newValue.format("mm:ss"));
+              }}
+            />
+          </LocalizationProvider>
+        )}
         <IconButton aria-label="delete" color="error">
           <DeleteIcon />
         </IconButton>
